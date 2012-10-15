d720133a0f707e648c9f267254895f162df36bd1
Justin Kamerman <junk@kaleco.net>
2012-10-15 11:36:08 -0300

    Refreshing code from unb repo

--- data/bin/useful_queries.sql
+++
@@ -1,3 +0,0 @@
-# Compare users followers count to the number of followers represented in the database
-select screen_name, followee_id, count(follower_id), followers_count, statuses_count from Follower, User where User.id = followee_id  group by followee_id order by count(follower_id) desc limit 100;
-
